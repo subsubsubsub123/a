11a2b5795c11891ab8e1ad3cfe64b97e817aa3e6
subsubsubsub123 <windaluffy4@gmail.com>
2025-06-08 20:29:36 +0300

    Delete src/main/java/com/example/tasktracker directory

--- src/main/java/com/example/tasktracker/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.example.tasktracker.repository;
-
-import com.example.tasktracker.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
